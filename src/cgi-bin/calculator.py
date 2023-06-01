319c28e9ff4953077d4d0091989373c77ad26b00
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-06-01 15:33:46 +0200

    modify cgi script to handle multiple calculation. Remove some useless css and align webserv title

--- src/cgi-bin/calculator.py
+++ src/cgi-bin/calculator.py
@@ -1,20 +1,45 @@
 #!/bin/python
 
-import cgi
+import cgi, cgitb
 
 form = cgi.FieldStorage()
 
+error = False
 number1 = form.getValue("num1")
 number2 = form.getValue("num2")
 operator = form.getValue("operator")
 
+if operator == "+":
+	result = number1 + number2
+elif operator == "-":
+	result = number1 - number2
+elif operator == "/":
+	if (number2 == 0):
+		error = True
+	else:
+		result = number1 / number2
+elif operator == "*":
+	result = number1 * number2
+else:
+	error = True
 
-print "Content-type:text/html\r\n\r\n"
-print "<html>"
-print "<head>"
-print "<title>Hello - Second CGI Program</title>"
-print "</head>"
-print "<body>"
-print "<h2>%d %s %d = un nombre</h2>" % (number1, number2, operator)
-print "</body>"
-print "</html>"
+if error:
+	print "Content-type:text/html\r\n\r\n"
+	print "<html>"
+	print "<head>"
+	print "<title>Hello - Second CGI Program</title>"
+	print "</head>"
+	print "<body>"
+	print "<h2>Error with the calculation</h2>"
+	print "</body>"
+	print "</html>"
+else:
+	print "Content-type:text/html\r\n\r\n"
+	print "<html>"
+	print "<head>"
+	print "<title>Hello - Second CGI Program</title>"
+	print "</head>"
+	print "<body>"
+	print "<h2>%d %s %d = %d</h2>" % (number1, operator, number2, result)
+	print "</body>"
+	print "</html>"
